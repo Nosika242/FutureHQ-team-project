1a6aa8b9d5a4fe6bd92014e96a5922045b793d50
unknown <ogbonnamitchel004@gmail.com>
2025-11-18 14:37:52 +0100

    Fix/ Accepted Incoming Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import CommentLayout from './pages/CommentLayout';
-import PostPage from './pages/PostPage';
-=======
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { getCurrentUser, User } from './services/api';
@@ -18,21 +14,12 @@ import LoadingSpinner from './components/LoadingSpinner';
 import './App.css';
 
 function App() {
->>>>>>> c37afe649b32a428d58c36248d5f7c38dee97aaa
 
   return (
-<<<<<<< HEAD
-    <div>
-      <PostPage />
-      <CommentLayout/>
-    </div>
-  );
-=======
     <>
       <TailwindTest />
     </>
   )
->>>>>>> c37afe649b32a428d58c36248d5f7c38dee97aaa
 }
 
 export default App
