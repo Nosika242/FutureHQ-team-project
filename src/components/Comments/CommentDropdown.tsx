15e01e375b53e05648b2634ebb4780912e897b2f
unknown <ogbonnamitchel004@gmail.com>
2025-11-18 15:39:44 +0100

    fit/ Styling comment Component

--- src/components/Comments/CommentDropdown.tsx
+++ src/components/Comments/CommentDropdown.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import GridDrop from "../assets/images/MoreDropdown.png"
+import GridDrop from "../../assets/images/MoreDropdown.png"
 
 interface Props {
   onEdit: () => void;
