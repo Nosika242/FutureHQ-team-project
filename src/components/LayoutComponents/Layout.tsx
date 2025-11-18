c95ef3c064efced6e1b93d7227d5eb79375a6e9c
Eze Chidera <derachukwueze@gmail.com>
2025-11-18 15:50:33 +0100

    fix: navbar import in layout fixed

--- src/components/LayoutComponents/Layout.tsx
+++ src/components/LayoutComponents/Layout.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import type { ReactNode } from "react";
-import Navbar from './Navbar';
+import Navbar from './navbar';
 import Sidebar from './Sidebar';
 
 interface LayoutProps {
